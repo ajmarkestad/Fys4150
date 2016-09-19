c601a9683178ec00c554fd4f83b87631fc4e6686
VidveiLarsen <vidvei.larsen@gmail.com>
2016-09-19 08:04:52 +0200

    Opprettet prosjekt 2 filer ++

---
+++ Project2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <fstream> // For reading to file
+#include <iomanip>
+#include <sstream>
+#include <time.h>
+#include <armadillo>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+
+
+
+
+
