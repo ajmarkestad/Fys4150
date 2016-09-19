7a6e000556ade452184ca2d8e05a2fe87ae93e38
VidveiLarsen <vidvei.larsen@gmail.com>
2016-09-19 09:16:27 +0200

    unittests inn i cpp filen

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -7,16 +7,38 @@
 #include <time.h>
 #include <armadillo>
 
+using namespace arma;
 using namespace std;
+
+
+bool unittest_largest_off_diagonal();
+bool unittest_orthogonality();
+bool unittest_correct_eigenvalues();
+void jacobis_method(mat,double);
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+
+    //UNIT TESTS
+    bool tests = True;
+    bool tests_pass = True;
+    try { unittest_correct_eigenvalues()}
+
+    try { unittest_largest_off_diagonal()}
+
+    try { unittest_orthogonality() }
+
+
+
     return 0;
 }
 
 
 
+void jacobis_method(mat, double tolerance = pow(10,-8))
+{
+
+}
 
 
 
