07dd1c62d22b6b5c20aca1e51d98df9583ac7e2e
VidveiLarsen <vidvei.larsen@gmail.com>
2016-10-14 15:50:39 +0200

    leser nå data fra filen, og lager passende planeter basert på input. Ser ut til å simulere bra over 10 år med alle planetene

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -4,9 +4,9 @@
 #include "ensemble.h"
 #include "solver.h"
 #include <ostream>
-#include <istream>
-#include <fstream>
 #include <sstream>
+#include <string>
+#include <fstream>
 using namespace std;
 ofstream ifile;
 void createparticles(Ensemble &system,int nargs, char **vargs);
@@ -14,7 +14,7 @@ void createparticles(Ensemble &system,int nargs, char **vargs);
 int main(int nargs, char **vargs)
 {
     double years =10;
-    int numTimesteps = 400000;
+    int numTimesteps = 10000;
     if(nargs<2){
         cout << "Incorrect usage!" << endl;
         cout << "Usage:$ ./Project3 <numTimesteps> <years> <planet1> <planet2> ..." << endl;
@@ -24,25 +24,18 @@ int main(int nargs, char **vargs)
         cout << "Negative years is not good!" << endl;
         return 1;
     }else{
-        double years = *vargs[1];
+        years = atof(vargs[1]);
     }
     if (vargs[2]<0){
         cout << "Negative timesteps is not good!" << endl;
         return 1;
     }else{
-        double numTimesteps = *vargs[2];
+        numTimesteps = atoi(vargs[2]);
     }
 
     Ensemble solarSystem;
-    Particle &sun = solarSystem.createParticle( vec3(0.0,0.0,0.0), vec3(0.0,0.0,0.0), 1.0 );
     createparticles(solarSystem, nargs, vargs);
-
     vector<Particle> &bodies = solarSystem.bodies();
-
-    for(int i = 0; i<bodies.size(); i++) {
-        Particle &body = bodies[i]; // Reference to this body
-        cout << "The position of this object is " << body.position << " with velocity " << body.velocity << endl;
-    }
 
     double dt = (double)years/(double)numTimesteps;
 
@@ -57,22 +50,53 @@ int main(int nargs, char **vargs)
     cout << "The final positions are: " << endl;
     for(int i = 0; i<bodies.size(); i++) {
         Particle &body = bodies[i]; // Reference to this body
-        cout << "The position of this object is " << body.position << "with distance from sun" << body.position.length() << " with velocity " << body.velocity << endl;
-    }
+        cout << "Final status for particle with position: " << body.position << "length: " << body.position.length() << " and velociy: " << body.velocity << " and mass: " << body.mass << endl;
+         }
+
 
 
      return 0;
+
+
 }
 
 
 void createparticles(Ensemble &system, int nargs, char **vargs){
-    ifile.open("initial_data.txt");
+    string line;
+    ifstream myfile ("initial_data.txt");
+    if(myfile.is_open()){
+        getline(myfile,line); //skips date
+        getline(myfile,line); //skips headerline
 
-    for (int i=0; i<nargs-2; i++){
-
-
-        system.createParticle( vec3(1.0, 0.0, 0.0), vec3(0.0, 2*M_PI, 0.0), 3e-6 );
+        while (getline(myfile,line))
+           {
+            istringstream iss(line);
+            string sub;
+            iss >> sub;
+            for(int i = 0; i<nargs-3; i++){
+                if(atoi(sub.c_str())==atoi(vargs[i+3])){
+                    vec3 position(0,0,0);
+                    vec3 velocity(0,0,0);
+                    iss >> sub;
+                    position(0)=atof(sub.c_str());
+                    iss >> sub;
+                    position(1)=atof(sub.c_str());
+                    iss >> sub;
+                    position(2)=atof(sub.c_str());
+                    iss >> sub;
+                    velocity(0)=atof(sub.c_str())*365;
+                    iss >> sub;
+                    velocity(1)=atof(sub.c_str())*365;
+                    iss >> sub;
+                    velocity(2)=atof(sub.c_str())*365;
+                    iss >> sub;
+                    system.createParticle( position, velocity, atof(sub.c_str()));
+                    cout << "Created a particle with initial position: " << position << "length: " << position.length() << " and velociy: " << velocity << " and mass: " << atof(sub.c_str()) << endl;
+                }
+            }
+           }
+        myfile.close();
+    }else{
+        cout << "Unable to open file" << endl;
     }
-    ifile.close();
-
 }
