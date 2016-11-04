da4ece93800f487540bc1061d6fcefc28a11bd48
ajmarkestad <ask_markestad@hotmail.com>
2016-11-04 12:40:27 +0100

    EN FUCKING CHARACTER FEILgit add -ugit add -ugit add -u!

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -11,6 +11,7 @@
 #include <fstream>
 #include <iomanip>
 #include "lib.h"
+#include "mpi.h"
 using namespace  std;
 
 ofstream ofile;
@@ -30,8 +31,9 @@ int main(int argc, char* argv[])
 {
     char *outfilename;
     long idum;
-    int **spin_matrix, n_spins, mcs, n_spins_squared;
-    double w[17], average[5], initial_temp, final_temp, E, M, temp_step;
+    int **spin_matrix, n_spins, mcs, n_spins_squared, my_rank, numprocs;
+    double w[17], average[5], initial_temp, final_temp, E, M, temp_step, total_average[5] ;
+
 
     // Read in output file, abort if there are too few command-line arguments
     if( argc <= 6 ){
@@ -40,18 +42,52 @@ int main(int argc, char* argv[])
                 "<int temperature steps>" << endl;
         exit(1);
     }
-    outfilename=argv[1];
-    ofile.open(outfilename);
+
+    MPI_Init (&argc, &argv);
+    MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
+    if (my_rank == 0 && argc <= 1) {
+        cout << "Bad Usage: " << argv[0] <<
+                " read output file" << endl;
+        exit(1);
+    }
+    if (my_rank == 0 && argc > 1) {
+        outfilename=argv[1];
+        ofile.open(outfilename);
+    }
     mcs = atoi(argv[2]);
     n_spins=atoi(argv[3]);
     initial_temp=atof(argv[4]);
     final_temp=atof(argv[5]);
-    temp_step=atoi(argv[6]);
+    temp_step=atof(argv[6]);
+
+
+
+    int no_intervalls = mcs/numprocs;
+    int myloop_begin = my_rank*no_intervalls + 1;
+    int myloop_end = (my_rank+1)*no_intervalls;
+    if ( (my_rank == numprocs-1) &&( myloop_end < mcs) ) myloop_end = mcs;
+
+    MPI_Bcast (&n_spins, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast (&initial_temp, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast (&final_temp, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast (&temp_step, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+
+
+
 
     //    Read in initial values such as size of lattice, temp and cycles
     spin_matrix = (int**) matrix(n_spins, n_spins, sizeof(int));
-    n_spins_squared = pow(n_spins,2);
-    idum = -1; // random starting point
+    n_spins_squared = n_spins*n_spins;
+    idum = -1-my_rank; // random starting point
+
+
+
+    double  TimeStart, TimeEnd, TotalTime;
+    TimeStart = MPI_Wtime();
+
+
     for ( double temperature = initial_temp; temperature <= final_temp; temperature+=temp_step){
         //    initialise energy and magnetization
         E = M = 0.;
@@ -62,17 +98,30 @@ int main(int argc, char* argv[])
         for( int i = 0; i < 5; i++) average[i] = 0.;
         initialize(n_spins, temperature, spin_matrix, E, M);
         // start Monte Carlo computation
-        for (int cycles = 1; cycles <= mcs; cycles++){
+        for (int cycles = myloop_begin; cycles <= myloop_end; cycles++){
             Metropolis(n_spins, n_spins_squared, idum, spin_matrix, E, M, w);
             // update expectation values
             average[0] += E;    average[1] += E*E;
             average[2] += M;    average[3] += M*M; average[4] += fabs(M);
         }
+        cout<< temperature << "  " << my_rank << endl;
+        MPI_Reduce(&average, &total_average, 5, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
         // print results
-        output(n_spins, mcs, temperature, average);
+        if ( my_rank == 0) {
+            output(n_spins, mcs, temperature, total_average);
+        }
     }
     free_matrix((void **) spin_matrix); // free memory
+
     ofile.close();  // close output file
+    TimeEnd = MPI_Wtime();
+    TotalTime = TimeEnd-TimeStart;
+    if ( my_rank == 0) {
+        cout << "Time = " <<  TotalTime  << " on number of processors: "  << numprocs  << endl;
+    }
+
+    // End MPI
+    MPI_Finalize ();
     return 0;
 }
 
