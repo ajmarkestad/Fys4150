ff2e8cec5629623a10851af9417855e6c192682a
ajmarkestad <ask_markestad@hotmail.com>
2016-12-01 13:24:53 +0100

    python ting

--- Project5/main.py
+++ Project5/main.py
@@ -7,14 +7,14 @@ from mpl_toolkits.mplot3d import Axes3D
 
 #Initialization test
 totalruns = 10000
-numberofbins = 400
-numberofagens = 400
+numberofbins = 8000
+numberofagens = 800
 initialcycles = 10000
-transactions = 1
-inputlambda = 0
+transactions = 10
+inputlambda = 0.25
 inputgamma = 0
 inputalpha = 0
-normalization = 0.001
+normalization = 0.1000
 command = "./Project5"+ " " + 'test' + " " + str(totalruns)+ " " + str(numberofbins) + " " + str(numberofagens) + " " + str(initialcycles)+ " " + str(transactions)+ " " + str(inputlambda)+ " " + str(inputgamma)+ " " + str(inputalpha)+ " " + str(normalization)
 
 print(command)
@@ -34,15 +34,14 @@ os.system('rm '+'test')
 
 #Proper run for simple transactions
 totalruns = 10000
-numberofbins = 400
-numberofagens = 400
+numberofbins = 8000
+numberofagens = 800
 initialcycles = 2000
 transactions = 10
-transactions = 1
-lambdainput = 0
+lambdainput = 0.25
 gammainput = 0
 alphainput = 0
-normalization = 1
+normalization = 0.1000
 command = "./Project5"+ " " + 'propersimple' + " " + str(totalruns)+ " " + str(numberofbins) + " " + str(numberofagens) + " " + str(initialcycles)+ " " + str(transactions)+ " " + str(inputlambda)+ " " + str(inputgamma)+ " " + str(inputalpha)+ " " + str(normalization)
 print(command)
 os.system(command)
@@ -53,7 +52,7 @@ for line in file1:
     count.append(float(line.split()[0]))
     bins.append(float(line.split()[1]))
 file1.close()
-plt.plot(bins[0:20],count[0:20])
+plt.plot(bins[0:30],count[0:30])
 plt.xlabel('Money')
 plt.ylabel('Count')
 plt.savefig("Proper_simple_transaction.png")
