bc366fa9f18aa559751019ae969d495bcd234a27
VidveiLarsen <vidvei.larsen@gmail.com>
2016-12-01 13:25:45 +0100

    loop over lambda

--- Project5/main.py
+++ Project5/main.py
@@ -38,7 +38,6 @@ numberofbins = 400
 numberofagens = 400
 initialcycles = 2000
 transactions = 10
-transactions = 1
 lambdainput = 0
 gammainput = 0
 alphainput = 0
@@ -49,6 +48,16 @@ os.system(command)
 file1 = open('propersimple','r')
 count = []
 bins = []
+
+file2 = open('propersimple','r')
+variance = []
+for line in file2:
+    variance.append(float(line))
+file2.close()
+plt.plot(variance)
+plt.xlabel('Transactions')
+plt.ylabel('Variance')
+
 for line in file1:
     count.append(float(line.split()[0]))
     bins.append(float(line.split()[1]))
@@ -59,5 +68,49 @@ plt.ylabel('Count')
 plt.savefig("Proper_simple_transaction.png")
 plt.close()
 os.system("rm "+"propersimpleinit")
+
+#Savings included
+
+totalruns = 10000
+numberofbins = 400
+numberofagens = 400
+initialcycles = 2000
+transactions = 10
+transactions = 1
+lambdainput = [0,0.25,0.9]
+gammainput = 0
+alphainput = 0
+normalization = 1
+for lambdaint in lambdainput:
+    command = "./Project5"+ " " + 'saving' + " " + str(totalruns)+ " " + str(numberofbins) + " " + str(numberofagens) + " " + str(initialcycles)+ " " + str(transactions)+ " " + str(lambdaint)+ " " + str(inputgamma)+ " " + str(inputalpha)+ " " + str(normalization)
+    print(command)
+    os.system(command)
+    file1 = open('saving','r')
+    count = []
+    bins = []
+    for line in file1:
+        count.append(float(line.split()[0]))
+        bins.append(float(line.split()[1]))
+    file1.close()
+    plt.plot(bins[0:20],count[0:20])
+    os.system("rm "+"savingint")
+    os.system("rm "+"savinginit")
+
+plt.show()
+plt.xlabel('Money')
+plt.ylabel('Count')
+plt.savefig("Saving.png")
+plt.close()
+
+
+file2 = open('saving','r')
+variance = []
+for line in file2:
+    variance.append(float(line))
+file2.close()
+plt.plot(variance)
+plt.xlabel('Transactions')
+plt.ylabel('Variance')
+
 
 
